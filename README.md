35a6fea91f3a89857d7c2854c3f5f5b2bf53af53
Chris Bautista <>
2019-09-23 18:05:24 -0700

    added nextjs

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # DojoAfterDark
-Ninjas
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import React from "react"
+
+const Home = () => {
+  console.log("Welcome to Dojo After Dark")
+
+  return (
+    <div>
+      <Head>
+        <title>Dojo After Dark</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Home
